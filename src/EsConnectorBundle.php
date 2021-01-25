b6cd3b982a7f1a75ca05233e30dccd438ced6e1c
Lars Nieuwenhuizen <lars@opengemeenten.nl>
2021-01-25 20:01:45 +0100

    dev: Create Bundle integration

---
+++ src/EsConnectorBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+namespace LarsNieuwenhuizen\EsConnector;
+
+use Symfony\Component\Console\Application;
+
+final class EsConnectorBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
+{
+}
